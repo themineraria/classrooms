f4991e0ef27ab434016c014cf673730b3c729923
Loulous <40513948+Loulous@users.noreply.github.com>
2018-06-23 21:02:58 +0200

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Classrooms - Créer un compte</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Créer un compte">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/classrooms-interface.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/socicon.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/lightbox.min.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/flickity.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/iconsmind.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/jquery.steps.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/theme.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/font-robotoslab.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700%7CMerriweather:300,300i" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+    <body class=" ">
+        <a id="start"></a>
+        <div class="nav-container ">
+            <nav class="bar bar-4 bar--transparent bar--absolute" data-fixed-at="200">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-1 col-md-2 col-md-offset-0 col-4">
+                            <div class="bar__module">
+                                <a href="index.php">
+                                    <img class="logo logo-dark" alt="logo" src="img/logo-dark.png" />
+                                    <img class="logo logo-light" alt="logo" src="img/logo-light.png" />
+                                </a>
+                            </div>
+                            <!--end module-->
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </nav>
+            <!--end bar-->
+        </div>
+        <div class="main-container">
+            <section class="imageblock switchable feature-large height-100">
+                <div class="imageblock__content col-lg-6 col-md-4 pos-right">
+                    <div class="background-image-holder">
+                        <img alt="image" src="img/hero-3.jpg" />
+                    </div>
+                </div>
+                <div class="container pos-vertical-center">
+                    <div class="row">
+                        <div class="col-lg-5 col-md-7">
+                            <h2>Création de votre compte</h2>
+                            <p class="lead">Essayez l'offre Premium durant 14 jours, aucun moyen de paiement requis.</p>
+                            <a class="btn block btn--icon bg--facebook type--uppercase" href="#">
+                                <span class="btn__text">
+                                    <i class="socicon-facebook"></i>
+                                    S'inscrire depuis Facebook
+                                </span>
+                            </a>
+                            <a class="btn block btn--icon bg--twitter type--uppercase" href="#">
+                                <span class="btn__text">
+                                    <i class="socicon-twitter"></i>
+                                    S'inscrire depuis Twitter
+                                </span>
+                            </a>
+                            <hr data-title="OU">
+                            <form>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <input type="email" name="Email Address" placeholder="E-Mail" />
+                                    </div>
+                                    <div class="col-12">
+                                        <input type="password" name="Password" placeholder="Mot de passe" />
+                                    </div>
+                                    <div class="col-12">
+                                        <button type="submit" class="btn btn--primary type--uppercase">Create Account</button>
+                                    </div>
+                                    <div class="col-12">
+                                        <span class="type--fine-print">En cliquant sur ce bouton, j'accepte les
+                                            <a href="#">Conditions d'utilisations</a>
+                                        </span>
+                                    </div>
+                                </div>
+                                <!--end row-->
+                            </form>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+        </div>
+        <!--<div class="loader"></div>-->
+        <a class="back-to-top inner-link" href="#start" data-scroll-class="100vh:active">
+            <i class="classrooms-interface classrooms-up-open-big"></i>
+        </a>
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/flickity.min.js"></script>
+        <script src="js/easypiechart.min.js"></script>
+        <script src="js/parallax.js"></script>
+        <script src="js/typed.min.js"></script>
+        <script src="js/datepicker.js"></script>
+        <script src="js/isotope.min.js"></script>
+        <script src="js/ytplayer.min.js"></script>
+        <script src="js/lightbox.min.js"></script>
+        <script src="js/granim.min.js"></script>
+        <script src="js/jquery.steps.min.js"></script>
+        <script src="js/countdown.min.js"></script>
+        <script src="js/twitterfetcher.min.js"></script>
+        <script src="js/spectragram.min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
